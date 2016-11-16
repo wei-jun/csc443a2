54f27891e3bbee2a980e1dfc1ef753a520272e40
wei jun zeng <weijun.zeng@mail.utoronto.ca>
2016-11-16 14:07:33 -0500

    add insert.cc

--- insert.cc
+++ insert.cc
@@ -35,22 +35,159 @@ int main(int argc, char* argv[])
 	}
 
 	Heapfile *heapfile = (Heapfile *)malloc(sizeof(Heapfile));
-	void init_heapfile(heapfile, page_size, heapfile_fp);
+	init_heapfile(heapfile, page_size, heapfile_fp);
 
 	char line[MAXLINE];  // line in csv file
 	char row[MAXLINE];   // row in page
 	bzero(line, MAXLINE);
 	bzero(row, MAXLINE);
-	// Record *record;
-	Slot *slot_ptr;
+	Record *record;
+	//Slot *slot_ptr;
 
+	PageID dir_page_id = 0;
+	PageID data_page_id = 1;
+	Page_entry *dir_page_entry, *data_page_entry;
+	int dir_page_capacity = page_size / (OFFSET_LEN + FREESPACE_LEN);
+	int data_page_capacity = page_size / sizeof(Slot);
+
+    // load the first directory page into memory
+	Page *dir_page = (Page *)malloc(sizeof(Page));
+	dir_page->data = (void *)malloc(page_size);
+	init_fixed_len_page(dir_page, page_size, OFFSET_LEN + FREESPACE_LEN);
+	read_page(heapfile, dir_page_id, dir_page);
+	dir_page_entry = (Page_entry *)dir_page->data;
+	data_page_entry = dir_page_entry + 1;
 	
+	// data_page for loading data page from heapfile into memory
+	Page *data_page = (Page *)malloc(sizeof(Page));
+	data_page->data = (void *)malloc(page_size);
+	init_fixed_len_page(data_page, page_size, sizeof(Slot));
+	read_page(heapfile, 1, data_page);
+
+	int rec_count = 0;
+	//int page_count = 0;	
+	struct timeb t_start, t_end;
+	long start_in_ms, end_in_ms;
+	int t_diff = 0;
+
+	ftime(&t_start);
+	start_in_ms = t_start.time * 1000 + t_start.millitm;
+
+	//printf("before reading csv_file\n");
+    // read csv file line by line
+	while(fgets(line, MAXLINE, csv_fp) != NULL) {
+		// printf("there is a line = %s\n", line);
+		char *curr_attr = strtok(line, ",");
+		printf("first attr = %s\n", curr_attr);
+		int i = 0;
+		// remove comma "," from the line and save into row
+		while (curr_attr) {
+			strncpy(row + i * ATTR_LEN, curr_attr, ATTR_LEN);
+			curr_attr = strtok(NULL, ",");
+			i++;			
+		}
+
+		printf("row = %s\n", row);
+
+		// save the row to a record
+		// fixed_len_read(row, sizeof(Slot) - 1, record);
+		fixed_len_read((void *)row, sizeof(Slot), record);
+
+		printf("after fixed_len_read\n");
 
 
+		rec_count++;
+		bzero(line, MAXLINE);
+	    bzero(row, MAXLINE);
 
+		// try to insert the record into the current data page
+		if (add_fixed_len_page(data_page, record) != -1) {
+			printf("record added in current data page\n");
+			data_page_entry->freespace--;
+		}
+		else {
+			// save current data page into disk
+			printf("before wirte current data page into disk\n");
+			write_page(data_page, heapfile, data_page_id);
+			// find a page with free slot in current dir page to insert the record
+			int found = 0;
+			int i;
+			for (i = 1; i < dir_page_capacity; i++) {
+				data_page_entry = dir_page_entry + i;
+				if ((data_page_entry->freespace != 0) && (data_page_entry->offset != 0)) {
+					data_page_id = data_page_entry->offset / heapfile->page_size;
+					found = 1;
+					printf("page with freespace found in current dir page\n");
+					break;
+				}
+			}
 
-	
+			// if data page with freespace not find in current dir page, search next dir page
+		    while ((found == 0) && (dir_page_entry->offset != 0)) {
+				int dir_page_id = dir_page_entry->offset / heapfile->page_size;
+				bzero(dir_page->data, heapfile->page_size);
+				read_page(heapfile, dir_page_id, dir_page);
+				dir_page_entry = (Page_entry *)dir_page->data;
 
+				for (i = 1; i < dir_page_capacity; i++) {
+					data_page_entry = dir_page_entry + i;
+					if ((data_page_entry->freespace != 0) && (data_page_entry->offset != 0)) {
+						data_page_id = data_page_entry->offset / heapfile->page_size;
+						found = 1;
+						break;
+					}
+				}
+			}
+
+			// if the data page with free slot found
+			if (found == 1) {
+				//read data page with data_page_id into memory, insert record
+				bzero(data_page->data, heapfile->page_size);
+				read_page(heapfile, data_page_id, data_page);
+				add_fixed_len_page(data_page, record);
+
+				// update dir page entry
+				data_page_entry->freespace--;
+
+			} 
+			if (found == 0) {    // data page with free slot not found
+				// save the current data page and dir page into disk
+				write_page(data_page, heapfile, data_page_id);
+				write_page(dir_page, heapfile, dir_page_id);
+				// allocate a new data page
+				data_page_id = alloc_page(heapfile);
+				bzero(data_page->data, heapfile->page_size);
+				read_page(heapfile, data_page_id, data_page);
+	            // reload dir page, it may different after allocating a new data page
+				dir_page_id = ((int)(data_page_id / dir_page_capacity)) * dir_page_capacity;
+				bzero(dir_page->data, heapfile->page_size);
+				read_page(heapfile, dir_page_id, dir_page);
+				dir_page_entry = (Page_entry *)dir_page->data;
+				data_page_entry = dir_page_entry + data_page_id % dir_page_capacity;
+
+				add_fixed_len_page(data_page, record);
+				data_page_entry->freespace--;
+			}
+		}
+	}
+
+		write_page(data_page, heapfile, data_page_id);
+		write_page(dir_page, heapfile, dir_page_id);
+
+		ftime(&t_end);
+		end_in_ms = t_end.time * 1000 + t_end.millitm;
+		t_diff = end_in_ms - start_in_ms; 
+
+		free(dir_page->data);
+		free(dir_page);
+		free(data_page->data);
+		free(data_page);
+		fclose(csv_fp);
+		fclose(heapfile_fp);
+
+		printf("NUMBER OF RECORDS: %d\n", rec_count);
+		printf("TIME: %d ms\n", t_diff);
 
 	return 0;
+
  }
